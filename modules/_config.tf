83c8aeb93d4d3820919824d5d9ac8884126394d7
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-14 15:43:32 +0000

    Fix api_url

--- modules/_config.tf
+++ modules/_config.tf
@@ -3,7 +3,7 @@ provider "datadog" {
   version = "~> 2.5"
   api_key = var.datadog_api_key
   app_key = var.testing_datadog_api
-  api_url = "https://api.datadoghq.eu/api/"
+  api_url = "https://api.datadoghq.eu/"
 }
 
 provider "aws" {
