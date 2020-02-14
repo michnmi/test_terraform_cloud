f328010feb88a4afa2106ac765c185a27e58c5ec
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-14 14:51:37 +0000

    test

--- modules/main.tf
+++ modules/main.tf
@@ -5,13 +5,11 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-035966e8adab4aaad"
   instance_type = "t2.micro"
-  
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, Kayla! Missed you!" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
+              echo "Hello, mate! Missed you!" > index.html
+              nohup busybox httpd -f -p 8080 &
               EOF
 
   tags = { 
