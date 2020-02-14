085a2bd67eb3c8844e87f6906652ea2d1f7eb1b4
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-14 14:50:23 +0000

    TEsT

--- modules/main.tf
+++ modules/main.tf
@@ -2,3 +2,20 @@ provider "aws" {
     region = "eu-west-1"
 }
 
+resource "aws_instance" "example" {
+  ami           = "ami-035966e8adab4aaad"
+  instance_type = "t2.micro"
+  
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, Kayla! Missed you!" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  tags = { 
+      Name = "terraform-example"
+  }
+
+}
