8f4a4a584ad08f8203c36cdf5077af79d324457b
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-14 16:01:52 +0000

    Testing removing ar esousre user

--- modules/main.tf
+++ modules/main.tf
@@ -1,10 +1,3 @@
-resource "datadog_user" "alex_podobnik" {
-  email       = "michalis@hashicorp.com"
-  handle      = "michalis@hashicorp.com"
-  name        = "MM"
-  access_role = "adm"
-}
-
 resource "datadog_user" "Martin_smith" {
   email       = "msmith@hashicorp.com"
   handle      = "msmith@hashicorp.com"
