2d5160ce926134b84457a6c57fde190610df1141
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-14 15:56:14 +0000

    Adding martin

--- modules/main.tf
+++ modules/main.tf
@@ -4,3 +4,10 @@ resource "datadog_user" "alex_podobnik" {
   name        = "MM"
   access_role = "adm"
 }
+
+resource "datadog_user" "Testing you can ignore me" {
+  email       = "msmith@hashicorp.com"
+  handle      = "msmith@hashicorp.com"
+  name        = "msmith@hashicorp.com"
+  access_role = "adm"
+}
