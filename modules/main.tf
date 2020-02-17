9a5ce491b8cca8af192a508407715230ad192a7f
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-17 09:22:11 +0000

    preaparing datadog to look like it is in prod

--- modules/main.tf
+++ modules/main.tf
@@ -1,13 +1,30 @@
 resource "datadog_user" "Martin_smith" {
-  email       = "msmith@hashicorp.com"
-  handle      = "msmith@hashicorp.com"
-  name        = "msmith@hashicorp.com"
+  email       = "msmith@hashicorp1.com"
+  handle      = "msmith@hashicorp1.com"
+  name        = "Martin_smith"
   access_role = "adm"
 }
 
-
-resource "datadog_user" "admin_users" {
-  count = length(var.user_names)
-  name = var.user_names[count.index]
+resource "datadog_user" "will_bengston" {
+  count       = local.env == "prod" ? 1 : 0
+  email       = "wbengtson@hashicorp1.com"
+  handle      = "wbengtson@hashicorp1.com"
+  name        = "Will Bengston"
+  access_role = "st"
 }
 
+resource "datadog_user" "zack_iles" {
+  count       = local.env == "prod" ? 1 : 0
+  email       = "zack.iles@hashicorp1.com"
+  handle      = "zack.iles@hashicorp1.com"
+  name        = "Zack Iles"
+  access_role = "st"
+}
+
+resource "datadog_user" "matt_mcquillan" {
+  count       = local.env == "prod" ? 1 : 0
+  email       = "matt@hashicorp1.com"
+  handle      = "matt@hashicorp1.com"
+  name        = "Matt McQuillan"
+  access_role = "adm"
+}
