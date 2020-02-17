8ac51c24a69999b5030c8ba5452d4cabe763666d
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-17 16:22:53 +0000

    Remove map function (just for adm_users)

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,16 +1,29 @@
 variable "datadog_api_key" {}
 variable "testing_datadog_api" {}
 
-variable "adm_users" {
-    "name" = string,
-    "email" = string,
-    "handler" = string,
-    "description" = "Admin user created by Terraform. Do not edit manually"
+# variable "adm_users" {
+#     type = map(object({
+#         name = string
+#         email = string
+#         handler = string
+#     }))
+#     description = "Admin user created by Terraform. Do not edit manually"
+# }
+
+variable "adm_users" { 
+    type = "map"
+    default = {
+        name = string
+        email = string
+        handler = string
+    }
 }
 
 variable "st_users" {
-    "name" = string,
-    "email" = string,
-    "handler" = string,
-    "description" = "ST user created by Terraform. Do not edit manually"
+    type = map(object({
+        name = string
+        email = string
+        handler = string
+    }))
+    description = "ST user created by Terraform. Do not edit manually"
 }
