940d191b7864947a2a4da34ed419820124648138
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-17 16:41:45 +0000

    Fixing deprecation warning

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,9 +2,9 @@ variable "datadog_api_key" {}
 variable "testing_datadog_api" {}
 
 variable "adm_users" { 
-    type = "map"
+    type = map
 }
 
 variable "st_users" {
-    type = "map"
+    type = map
 }
