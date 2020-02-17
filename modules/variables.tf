24475900baaa4a7156ef078876c938b3d1907e7b
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-17 16:17:13 +0000

    Remove map

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,19 +2,15 @@ variable "datadog_api_key" {}
 variable "testing_datadog_api" {}
 
 variable "adm_users" {
-    type = map(object({
-        name = string
-        email = string
-        handler = string
-    }))
+    name = string
+    email = string
+    handler = string
     description = "Admin user created by Terraform. Do not edit manually"
 }
 
 variable "st_users" {
-    type = map(object({
-        name = string
-        email = string
-        handler = string
-    }))
+    name = string
+    email = string
+    handler = string
     description = "ST user created by Terraform. Do not edit manually"
 }
