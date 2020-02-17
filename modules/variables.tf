3a4574fdf97f1e696c4a828a126556354b6d62e3
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-17 16:19:03 +0000

    Possible typo #2

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,15 +2,15 @@ variable "datadog_api_key" {}
 variable "testing_datadog_api" {}
 
 variable "adm_users" {
-    name = string,
-    email = string,
-    handler = string,
-    description = "Admin user created by Terraform. Do not edit manually"
+    "name" = string,
+    "email" = string,
+    "handler" = string,
+    "description" = "Admin user created by Terraform. Do not edit manually"
 }
 
 variable "st_users" {
-    name = string,
-    email = string,
-    handler = string,
-    description = "ST user created by Terraform. Do not edit manually"
+    "name" = string,
+    "email" = string,
+    "handler" = string,
+    "description" = "ST user created by Terraform. Do not edit manually"
 }
