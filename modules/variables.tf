c6a1589820a77425e24f03b36f0ba6b68d1e0081
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-17 16:38:10 +0000

    Creating variables needed to load users as dictionaries

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,10 +1,10 @@
 variable "datadog_api_key" {}
 variable "testing_datadog_api" {}
 
-# variable "adm_users" { 
-#     type = "map"
-# }
+variable "adm_users" { 
+    type = "map"
+}
 
-# variable "st_users" {
-#     type = "map"
-# }
+variable "st_users" {
+    type = "map"
+}
