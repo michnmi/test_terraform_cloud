a6ea9a2b59267132245e8f7afa6e72a2368af1bf
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-17 09:23:05 +0000

    Fixing typo

--- modules/main.tf
+++ modules/main.tf
@@ -6,7 +6,6 @@ resource "datadog_user" "Martin_smith" {
 }
 
 resource "datadog_user" "will_bengston" {
-  count       = local.env == "prod" ? 1 : 0
   email       = "wbengtson@hashicorp1.com"
   handle      = "wbengtson@hashicorp1.com"
   name        = "Will Bengston"
@@ -14,7 +13,6 @@ resource "datadog_user" "will_bengston" {
 }
 
 resource "datadog_user" "zack_iles" {
-  count       = local.env == "prod" ? 1 : 0
   email       = "zack.iles@hashicorp1.com"
   handle      = "zack.iles@hashicorp1.com"
   name        = "Zack Iles"
@@ -22,7 +20,6 @@ resource "datadog_user" "zack_iles" {
 }
 
 resource "datadog_user" "matt_mcquillan" {
-  count       = local.env == "prod" ? 1 : 0
   email       = "matt@hashicorp1.com"
   handle      = "matt@hashicorp1.com"
   name        = "Matt McQuillan"
