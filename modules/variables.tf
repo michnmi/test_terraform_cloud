69cbf7012d8a00a12f1381bb99f0f1e9bdb4279b
Michalis Michaloliakos <michalis@hashicorp.com>
2020-02-17 10:37:27 +0000

    double quotes

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,7 +1,7 @@
 variable "datadog_api_key" {}
 variable "testing_datadog_api" {}
 
-variable adm_users {
+variable "adm_users" {
     type = map(object({
         name = string
         email = string
