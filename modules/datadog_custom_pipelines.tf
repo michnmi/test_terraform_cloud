ed93e14e6a90a5874855980cb6ea1d9d92b2813c
Michalis Michaloliakos <michalis@hashicorp.com>
2020-03-20 14:26:23 +0000

    Adding another custom pipeline (cadence-history)

--- modules/datadog_custom_pipelines.tf
+++ modules/datadog_custom_pipelines.tf
@@ -70,3 +70,51 @@ resource "datadog_logs_custom_pipeline" "cadence-worker_cadence-shared" {
         }
     }
 }
+
+resource "datadog_logs_custom_pipeline" "cadence-matching_and_cadence-history" {
+    filter {
+        query = "service:(cadence-matching OR cadence-server)"
+    }
+    name = "cadence-matching and cadence-history"
+    is_enabled = true
+    processor {
+        pipeline {
+            is_enabled = true
+            filter {
+                query = "@msg:none"
+            }
+            name = "msg set to \"none\""
+            processor {
+                message_remapper {
+                    name = "Define lifecycle as official Message field"
+                    is_enabled = true
+                    sources = ["lifecycle"]
+                }
+            }
+        }
+    }
+    processor {
+        message_remapper {
+            is_enabled = true
+            sources = ["msg"]
+            name = "Define msg as official Message field"
+        }
+    }
+    processor {
+        date_remapper {
+            is_enabled = true
+            sources = ["ts"]
+            name = "Define ts as the date attribute"
+        }
+    }
+    processor {
+        status_remapper {
+            is_enabled = true
+            sources = ["level"]
+            name = "Define level as status attribute"
+        }
+    }
+
+
+
+
