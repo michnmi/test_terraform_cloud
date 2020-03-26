ac7755d830b084375fdad95871031ca770661d4d
Michalis Michaloliakos <michalis@hashicorp.com>
2020-03-26 09:45:59 +0000

    Adding consul-ama blah blah

--- modules/datadog_custom_pipelines.tf
+++ modules/datadog_custom_pipelines.tf
@@ -294,3 +294,50 @@ resource "datadog_logs_custom_pipeline" "Traefik_with_unquoted_access_log_fixes_
         }
     }
 }
+resource "datadog_logs_custom_pipeline" "cloud_consul-ama,operation,provider,blob,data-plane-config,network,region,consul-service,host-manager-service,image" {
+    filter {
+        query = "service:(cloud-consul-ama OR cloud-operation OR cloud-provider OR cloud-blob OR cloud-data-plane-config OR cloud-network OR cloud-region OR cloud-consul-service OR cloud-image-service OR cloud-host-manager-service)"
+    }
+    name = "cloud: consul-ama, operation, provider, blob, data-plane-config, network, region, consul-service, host-manager-service, image"
+    is_enabled = true
+    processor {
+        message_remapper {
+            name = "Assign msg, message to official Message field"
+            is_enabled = true
+            sources = ["msg", "@message"]
+        }
+    }
+    processor {
+        grok_parser {
+            name = "Date, level, msg processor"
+            is_enabled = true
+            source = "message"
+            # samples = ["172.17.0.1 - - [19/Dec/2019:15:23:37 +0000] "POST /consul/2019-11-20/.internal/clusters/11ea2272-5dd8-7571-93ec-0242ac110003/host-alive HTTP/1.1" 200 26 "" "Go-http-client/1.1"", "2019-12-18T20:59:28.451Z [WARN]  consul-ama.grpc: WARNING: 2019/12/18 20:59:28 grpc: Server.Serve failed to create ServerTransport:  connection error: desc = "transport: http2Server.HandleStreams failed to receive the preface from client: EOF"", "172.17.0.1 - - [11/Feb/2020:09:40:07 +0000] "GET /consulama/2019-09-10/subscriptions/02dd1374-6542-4060-8dd7-75d686331850/resourceGroups/hourlytest-prod-1233/providers/Microsoft.CustomProviders/resourceProviders/public/consulClusters/test HTTP/1.1" 202 434 "" """, "WARNING: 2020/03/23 16:46:10 grpc: Server.Serve failed to create ServerTransport:  connection error: desc = "transport: http2Server.HandleStreams failed to receive the preface from client: read tcp 172.17.0.9:28080->10.0.64.61:56492: read: connection reset by peer""]
+            # grok = "{'support_rules': '', 'match_rules': 'rule_syslog %{date("yyyy-MM-dd\'T\'HH:mm:ss.SSSZ"):date} \\[%{word:level}\\] +%{notSpace:component}: %{data:msg}\nrule_syslog2 %{word:level}: %{date("yyyy/MM/dd HH:mm:ss"):date} +%{notSpace:component}: %{data:msg}\nrule_apache_common %{ipv4:network.client.ip}\\s+-\\s+-\\s+\\[%{date("dd/MMM/yyyy:HH:mm:ss Z"):date}\\]\\s+\\"%{word:http.method}\\s+%{notSpace:http.url}\\s+%{notSpace:http.version}\\"\\s+%{integer:http.status_code}\\s+%{integer:bytes}\\s+\\"\\"\\s+\\"%{notSpace:http.user_agent}?\\"'}"
+            samples = [
+                "172.17.0.1 - - [19/Dec/2019:15:23:37 +0000] \"POST /consul/2019-11-20/.internal/clusters/11ea2272-5dd8-7571-93ec-0242ac110003/host-alive HTTP/1.1\" 200 26 \"\" \"Go-http-client/1.1\"",
+                "2019-12-18T20:59:28.451Z [WARN]  consul-ama.grpc: WARNING: 2019/12/18 20:59:28 grpc: Server.Serve failed to create ServerTransport:  connection error: desc = \"transport: http2Server.HandleStreams failed to receive the preface from client: EOF\"",
+                "172.17.0.1 - - [11/Feb/2020:09:40:07 +0000] \"GET /consulama/2019-09-10/subscriptions/02dd1374-6542-4060-8dd7-75d686331850/resourceGroups/hourlytest-prod-1233/providers/Microsoft.CustomProviders/resourceProviders/public/consulClusters/test HTTP/1.1\" 202 434 \"\" \"\"",
+                "WARNING: 2020/03/23 16:46:10 grpc: Server.Serve failed to create ServerTransport:  connection error: desc = \"transport: http2Server.HandleStreams failed to receive the preface from client: read tcp 172.17.0.9:28080->10.0.64.61:56492: read: connection reset by peer\""
+            ]
+            grok {
+                support_rules = ""
+                match_rules = "rule_syslog %%{date(\"yyyy-MM-dd'T'HH:mm:ss.SSSZ\"):date} \\[%%{word:level}\\] +%%{notSpace:component}: %%{data:msg}\nrule_syslog2 %%{word:level}: %%{date(\"yyyy/MM/dd HH:mm:ss\"):date} +%%{notSpace:component}: %%{data:msg}\nrule_apache_common %%{ipv4:network.client.ip}\\s+-\\s+-\\s+\\[%%{date(\"dd/MMM/yyyy:HH:mm:ss Z\"):date}\\]\\s+\\\"%%{word:http.method}\\s+%%{notSpace:http.url}\\s+%%{notSpace:http.version}\\\"\\s+%%{integer:http.status_code}\\s+%%{integer:bytes}\\s+\\\"\\\"\\s+\\\"%%{notSpace:http.user_agent}?\\\""
+            }
+        }
+    }
+    processor {
+        date_remapper {
+            name = "Assign date to official Date field"
+            is_enabled = true
+            sources = ["date", "@timestamp"]
+        }
+    }
+    processor {
+        status_remapper {
+            name = "Assign level to official Status field"
+            is_enabled = true
+            sources = ["level", "@level"]
+        }
+    }
+}
